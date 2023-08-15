3666a51d2fa14d89231c1aee60f0ea32335c0fad
dumpier <dumpieer@gmail.com>
2023-08-15 10:52:45 +0900

    税金計算

--- res/js/my.js
+++ res/js/my.js
@@ -1,20 +1,19 @@
-const $ = function(selector){
-    return selector ? myjs.create(selector) : myjs;
-};
-
 const myjs = {
-    els:null,
-    type:function(obj){ obj = obj || this.els; return Object.prototype.toString.call(obj).slice(8,-1) || typeof(obj); },
+    elements:null,
+    get:function(i){ return i===undefined ? this.elements : this.elements[i]; },
+    scan:function() {},
+    type:function(obj){ obj = obj || this.elements; return Object.prototype.toString.call(obj).slice(8,-1) || typeof(obj); },
     isHtmlElement:function( obj ){ return this.type(obj).match(/^HTML.+Element$/) ? true:false; },
-    current:function(){ return this.type()=="NodeList" ? this.els[0] : this.els; },
-
-    param:(name)=>{ return (new URL(window.location.href)).searchParams.get(name); },
+    current:function(){ return this.type()=="NodeList" ? this.elements[0] : this.elements; },
     create:(selector)=>{
         const obj = Object.create(myjs);
-        // TODO $(this)=>NodeList or HtmlxxxElement
-        obj.els = myjs.type(selector)=="String" ? document.querySelectorAll(selector) : selector;
+        obj.elements = myjs.type(selector)=="String" ? document.querySelectorAll(selector):selector;
         return obj;
     },
+    param:(name)=>{ return (new URL(window.location.href)).searchParams.get(name); },
+
+    show:function(){ this.current().style.display = "block"; },
+    hide:function(){ this.current().style.display = "none"; },
     html:function(html){
         if(!html) return this.current().innerHTML;
         this.current().innerHTML = html;
@@ -25,7 +24,7 @@ const myjs = {
     remove:function(selector){},
 
     first:function(){ return myjs.create(this.current()); },
-    eq:function(i){ return myjs.create(this.els[i]); },
+    eq:function(i){ return myjs.create(this.elements[i]); },
     find:function(sector){  },
     children:function(sector){  },
     parent:function(sector){  },
@@ -39,8 +38,8 @@ const myjs = {
     val:function(value){},
     css:function(value){},
 
-    on:function(type, handle) { this.els.forEach(function(el){el.addEventListener(type, handle, false);}); return this; },
-    off:function(type, handle){ this.els.forEach(function(el){el.removeEventListener(type, handle, false);}); return this; },
+    on:function(type, handle) { this.elements.forEach(function(el){el.addEventListener(type, handle, false);}); return this; },
+    off:function(type, handle){ this.elements.forEach(function(el){el.removeEventListener(type, handle, false);}); return this; },
 };
 
 myjs.attribute = {
@@ -69,4 +68,3 @@ myjs.css.class = {
     toggle:function(name){ this.current().classList.toggle(name); return this; },
     replace:function(name, new_name){ this.current().classList.replace(name, new_name); return this; },
 };
-
